45a9e707adc9a353f7145c4ad9c06bea1734549d
Raman Goyal <ramangoyal@192.168.0.2>
2021-07-19 22:42:07 +0530

    navigating between the routes

--- src/components/nav/TheNavigation.vue
+++ src/components/nav/TheNavigation.vue
@@ -3,10 +3,10 @@
     <nav>
       <ul>
         <li>
-          <button @click="setActivePage('teams-list')">Teams</button>
+          <router-link to='/teams'>Teams</router-link>
         </li>
         <li>
-          <button @click="setActivePage('users-list')">Users</button>
+          <router-link to='/users'>Users</router-link>
         </li>
       </ul>
     </nav>
@@ -17,9 +17,7 @@
 export default {
   emits: ['set-page'],
   methods: {
-    setActivePage(page) {
-      this.$emit('set-page', page);
-    },
+
   },
 };
 </script>
@@ -49,8 +47,8 @@ li {
   margin: 0 2rem;
 }
 
-button {
-  font: inherit;
+a {
+  text-decoration: none;
   background: transparent;
   border: 1px solid transparent;
   cursor: pointer;
@@ -59,8 +57,8 @@ button {
   display: inline-block;
 }
 
-button:hover,
-button:active {
+a:hover,
+a:active {
   color: #f1a80a;
   border-color: #f1a80a;
   background-color: #1a037e;
