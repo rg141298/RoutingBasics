ae75c98758bfafc6b71de38d7df79e32aeea54a6
Raman Goyal <ramangoyal@192.168.0.2>
2021-07-21 16:25:43 +0530

    Named Routes

--- src/main.js
+++ src/main.js
@@ -12,9 +12,14 @@ const router = createRouter({
   history: createWebHistory(),
   routes:[
     {path:'/', redirect:'/teams'},
-    { path: '/teams', component: TeamsList,children:[
-        { path:'/teams/:teamId', component:TeamMembers, props:true},
-      ]},
+    {
+      name:'teams',
+      path: '/teams',
+      component: TeamsList,
+      children:[
+        { name:'team-members', path:'/teams/:teamId', component:TeamMembers, props:true},
+      ]
+    },
     { path: '/users', component: UsersList},
     {path:'/:notFound(.*)',component:NotFound}
     ],
