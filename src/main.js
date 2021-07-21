6e608dfd2a92d9591ff178fae611ad559312b516
Raman Goyal <ramangoyal@192.168.0.2>
2021-07-21 17:35:34 +0530

    add multiple routes using named router-view tags and registring mulitiple components instead of single component in router cofiguration

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import TeamsList from '@/components/teams/TeamsList.vue';
 import UsersList from '@/components/users/UsersList.vue';
 import TeamMembers from '@/components/teams/TeamMembers.vue';
 import NotFound from '@/components/nav/NotFound.vue';
+import TeamsFooter from '@/components/teams/TeamsFooter.vue';
+import UsersFooter from '@/components/users/UsersFooter.vue';
+
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,12 +18,21 @@ const router = createRouter({
     {
       name:'teams',
       path: '/teams',
-      component: TeamsList,
+      components:
+        { default: TeamsList,
+          footer:TeamsFooter
+        },
       children:[
         { name:'team-members', path:'/teams/:teamId', component:TeamMembers, props:true},
       ]
     },
-    { path: '/users', component: UsersList},
+    {
+      path: '/users',
+      components:
+        { default: UsersList,
+          footer:UsersFooter
+        },
+    },
     {path:'/:notFound(.*)',component:NotFound}
     ],
 });
